ea358e02ad4d30f730233103b9998484cf4f3216
Rushil2812 <20it164@charusat.edu.in>
2022-04-07 01:31:20 +0530

    Frontend

--- src/Componemts/Askheader.js
+++ src/Componemts/Askheader.js
@@ -1,15 +1,21 @@
 import './Css/Askheader.css'
-import React from 'react';
+import React,{useState} from 'react';
 import Homeicon from '@material-ui/icons/Home'
 import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList'
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn'
 import NotificationsOutlined from '@material-ui/icons/NotificationsOutlined'
 import PeopleAltOutlined from '@material-ui/icons/PeopleAltOutlined'
 import Search from '@material-ui/icons/Search'
-import { Avatar, Button } from '@material-ui/core';
-
+import { Avatar, Button, Input } from '@material-ui/core';
+import Modal from 'react-responsive-modal';
+import CloseIcon from '@material-ui/icons/Close'
+import 'react-responsive-modal/styles.css'
+import { ExpandMore } from '@material-ui/icons';
 
 function Askheader() {
+    const [isModalOpen,setIsModalOpen]=useState(false);
+    const [inputUrl,setInputUrl]=useState("");
+    const Close=<CloseIcon />;
   return(
   <div className='aHeader'>
     <div className="aHeader-content">
@@ -30,7 +36,69 @@ function Askheader() {
             <div className="aHeader_Rem">
                 <Avatar />
             </div>
-            <Button>Ask Question</Button>
+            <Button onClick={()=>setIsModalOpen(true)}>Ask Question</Button>
+            <Modal open={isModalOpen} 
+            closeIcon = {Close} 
+            onClose={()=>setIsModalOpen(false)} 
+            closeOnEsc
+            center
+            closeOnOverlayClick={false}
+            styles={
+                {
+                    overlay:{
+                        height:"auto",
+                    }
+                }
+            }
+            >
+                <div className='modal__title'>
+                    <h5>Add Question</h5>
+                    <h5>Share Link</h5>
+                </div>
+                <div className="modal__info">
+                    <Avatar className='avatar'/>
+                    <div className='modal__scope'>
+                        <PeopleAltOutlined />
+                        <p>Public</p>
+                        <ExpandMore />
+                    </div>
+                </div>
+                <div className="modal__Field">
+                    <Input type="text" placeholder="Type Your Question"/>
+                    <div styles={{
+                        display:"flex",
+                        flexdirection:"column"
+                    }}>
+                        <input type='text'
+                        className='modal__fieldLink'
+                        value={inputUrl}
+                        onChange={(e)=>setInputUrl(e.target.value)}
+                        style={{
+                            margin:"5px 0",
+                            border:"1px solid lightgray",
+                            padding:"10px",
+                            outline:"2px solid black",
+                            width:"100%",
+                        }} 
+                        placeholder='Incldue link of image if you wish' />
+                        {
+                        inputUrl !== "" &&<img styles={{
+                            height:"40vh",
+                            objectFit:"contain",
+                        }}
+                        src={inputUrl} alt='image'></img>
+                        }   
+                    </div>
+                </div>
+                <div className="modal__buttons">
+                    <button className='cancle' onClick={()=>setIsModalOpen(false)}>
+                        Cancle
+                    </button>
+                    <button type='submit' className='add'>
+                        Add Question
+                    </button>
+                </div>
+            </Modal>
     </div>
   </div>
   )
--- src/Componemts/Post.js
+++ src/Componemts/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './Css/post.css'
 import { Avatar } from "@material-ui/core";
 import { ArrowUpwardOutlined } from "@material-ui/icons";
@@ -7,37 +7,75 @@ import { RepeatOneOutlined } from "@material-ui/icons";
 import { ChatBubbleOutlined } from "@material-ui/icons";
 import { ShareOutlined } from "@material-ui/icons";
 import { MoreHorizOutlined } from "@material-ui/icons";
+import Modal from 'react-responsive-modal';
+import CloseIcon from '@material-ui/icons/Close'
+import 'react-responsive-modal/styles.css'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 function Post() {
+    const [isModalOpen,setIsModalOpen]=useState(false);
+    const Close=<CloseIcon />;
   return (
     <div className='post'>
-        <div className='post_info'>
+        <div className='post__info'>
             <Avatar />
             <h4>User name</h4>
             <small>Timestamp</small>
         </div>
-        <div className='post_body'>
+        <div className='post__body'>
             <p>
             Test Question
             </p>
-            <button className='post_btn'>Answer</button>
+            <button onClick={()=>setIsModalOpen(true)}className='post_btn'>Answer</button>
+            <Modal
+            open={isModalOpen} 
+            closeIcon = {Close} 
+            onClose={()=>setIsModalOpen(false)} 
+            closeOnEsc
+            center
+            closeOnOverlayClick={false}
+            styles={
+                {
+                    overlay:{
+                        height:"auto",
+                    }
+                }
+            }
+            >
+                <div className="modal__question">
+                    <h1>This is Test Question.</h1>
+                    <p>asked by {" "} <span className="name">Username</span>on<span className="name">Timestamp</span></p>
+                </div>
+                <div className="modal__answer">
+                    <ReactQuill placeholder='Enter your answer' />
+                </div>
+                <div className="modal__button">
+                <button className='cancle' onClick={()=>setIsModalOpen(false)}>
+                        Cancle
+                    </button>
+                    <button type='submit' className='add'>
+                        Add Question
+                    </button>
+                </div>
+            </Modal>
         </div>
-        <div className='post_footer'>
-            <div className='post_action'>
+        <div className='post__footer'>
+            <div className='post__footerAction'>
                 <ArrowUpwardOutlined />
                 <ArrowDownwardOutlined />
             </div>
             <RepeatOneOutlined />
             <ChatBubbleOutlined />
-            <div className="post_footerright">
+            <div className="post__footerright">
                 <ShareOutlined />
                 <MoreHorizOutlined />
             </div>
         </div>
         <p>1 Answer</p>
-        <div className="post_answer">
-            <div className="post_answercontainer">
-                <div className="post_answerd">
+        <div className="post__answer">
+            <div className="post-answer-container">
+                <div className="post-answerd">
                     <Avatar />
                         <div className="post-info">
                             <p>
